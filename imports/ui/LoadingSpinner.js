fa05c63caef8e4ce7ab0bb88474d88b79573d5a1
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2016-06-11 18:00:58 -0400

    added loading while call to navigator goes

---
+++ imports/ui/LoadingSpinner.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class LoadingSpinner extends Component {
+  render() {
+    return (
+      <div>Loading...</div>
+    );
+  }
+}
--- imports/ui/Map.js
+++ imports/ui/Map.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 
+import LoadingSpinner from './LoadingSpinner';
+
 export default class Map extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      isLoaded: false,
+    };
     console.log(google);
+  }
+
+  componentDidMount() {
+    // get geolocation
+    // http://www.w3schools.com/html/html5_geolocation.asp
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position.coords);
+        this.setState({ isLoaded: true });
+      });
+    } else {
+      console.log('geolocation is not supported');
+    }
   }
 
   render() {
     return (
-      <div id="map"></div>
+      <div id="map-container">
+        {this.state.isLoaded ? <div id="map"></div> : <LoadingSpinner />}
+      </div>
     );
   }
 }
