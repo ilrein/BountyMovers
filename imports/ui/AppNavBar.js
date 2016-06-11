50b9918fd1162fa89fd0722a033c5761a33134e1
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2016-06-11 14:25:01 -0400

    just added functionality to the sidebar, now toggles

--- imports/ui/AppNavBar.js
+++ imports/ui/AppNavBar.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 
-const AppNavBar = () => (
-  <AppBar
-    title="Title"
-  />
-);
+// Sidebar
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+class AppNavBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { open: false };
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle() {
+    this.setState({ open: !this.state.open });
+  }
+
+  render() {
+    return (
+      <AppBar
+        title="BountyMovers"
+        onLeftIconButtonTouchTap={this.handleToggle}
+      >
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({ open })}
+        >
+          <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
+          <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
+        </Drawer>
+      </AppBar>
+    );
+  }
+}
 
 export default AppNavBar;
